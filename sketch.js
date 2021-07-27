08dd08c7c577535eaa18cf313b7dcd2d7ccb61d0
Prathmesh130000 <86115624+Prathmesh130000@users.noreply.github.com>
2021-07-27 21:20:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+function preload(){
+
+  seaImg = loadImage("sea.png");
+  shipImg1 =loadAnimation
+
+  spriteName.addImage(seaImg);
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+}
+
+
+
+function draw() {
+ 
+
+//code to reset the background
+if(sea.x <0){
+sea.x = sea.width/2;
+
+}
+}
+
+
+
+
+
+
